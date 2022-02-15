e5a088b685b5f83a933e8b9e0448ef0023bfdfbe
herbanarchitech <jeff.badette@protonmail.com>
2022-02-15 13:48:47 -0500

    very first bash script. 99 more days to have more fun with BASH

---
+++ hello_world.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+#Very rudimentary script that takes a single arguement from the user then writes it out to the console.
+
+# Enter only one arguement then exececute the script so it can on the console.
+
+
+username=$1
+
+echo " Hello $username,and Hello World, you have basically written your first script." 
