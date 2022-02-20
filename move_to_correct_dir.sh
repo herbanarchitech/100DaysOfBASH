861e4af50a60e1596ca790c48b02768fb2da0fdd
herbanarhitect <jeff.badette@protonmail.com>
2022-02-20 01:22:08 +0000

    script is funtioning as intended

---
+++ move_to_correct_dir.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#script to move txt file format to a dedicated txt directory which is Text
+
+for FN in *.txt 
+do
+
+mv  $FN /home/vagrant/home/Documents/Bash101/TestDir
+
+done
+
+
+ 
